990c0edc24d9fcbe7360107de04cb18529552bc1
Iván López <lopez.ivan@gmail.com>
2023-04-14 19:15:05 +0200

    Initial commit

---
+++ src/main/java/com/codemotion/demo/CodemotionMadrid2023Application.java
@@ -0,0 +1,13 @@
+package com.codemotion.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemotionMadrid2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodemotionMadrid2023Application.class, args);
+    }
+
+}
