17a3cd7d5044d11fc4d9a79f9f0ededac2b6b00c
Iván López <lopez.ivan@gmail.com>
2023-04-29 21:26:04 +0200

    Upgrade to Spring Boot 3.1.0-RC1

--- src/test/java/com/codemotion/demo/AbstractContainerTest.java
+++ src/test/java/com/codemotion/demo/AbstractContainerTest.java
@@ -6,35 +6,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.lifecycle.Startables;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(
-    webEnvironment = WebEnvironment.RANDOM_PORT,
-    properties = {"spring.datasource.url=jdbc:tc:postgresql:15.2:///"}
+    webEnvironment = WebEnvironment.RANDOM_PORT
 )
+@Testcontainers
 public abstract class AbstractContainerTest {
 
+  @Container
+  @ServiceConnection
   static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(DockerImageName.parse("postgres:15.2"));
 
+  @Container
+  @ServiceConnection
   static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.5"));
 
-  @DynamicPropertySource
-  static void init(DynamicPropertyRegistry registry) {
-    Startables.deepStart(postgreSQLContainer, kafka).join();
-
-    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-
-    registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-  }
+  //  @DynamicPropertySource
+  //  static void init(DynamicPropertyRegistry registry) {
+  //    Startables.deepStart(postgreSQLContainer, kafka).join();
+  //
+  //    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+  //    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+  //    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+  //
+  //    registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+  //  }
 
   protected RequestSpecification requestSpecification;
 
